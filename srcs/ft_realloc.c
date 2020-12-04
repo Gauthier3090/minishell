afc8eb5060c4a4d5b3fb8bdf230030bfbb9dde8f
Gauthier Pladet <gpladet@MacBook-Pro-de-Gauthier.local>
2020-12-04 20:01:05 +0100

    function realloc

---
+++ srcs/ft_realloc.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpladet <gpladet@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/04 19:19:46 by gpladet           #+#    #+#             */
+/*   Updated: 2020/12/04 20:00:23 by gpladet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/header.h"
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = -1;
+	while (src[++i])
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+void	*ft_realloc(void *ptr, size_t size)
+{
+	void *new;
+
+	if (!ptr)
+		return (NULL);
+	if (!size)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!(new = malloc(size)))
+		return (NULL);
+	new = ft_strcpy(new, ptr);
+	free(ptr);
+	return (new);
+}
