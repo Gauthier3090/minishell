f8e3f3329530d500dc2d308065627a44057d69a1
Gauthier3090 <gauthier.pladet@gmail.com>
2021-03-05 15:33:23 +0100

    norm

--- includes/header.h
+++ includes/header.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   header.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldavids <ldavids@student.s19.be>           +#+  +:+       +#+        */
+/*   By: gpladet <gpladet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 14:52:25 by gpladet           #+#    #+#             */
-/*   Updated: 2021/03/05 14:43:25 by ldavids          ###   ########.fr       */
+/*   Updated: 2021/03/05 15:31:22 by gpladet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -362,7 +362,8 @@ char				*empty_input(char *final_str);
 */
 void				init_shell_arg(t_minishell *shell);
 int					ft_builtins(t_minishell *shell, t_struct *glo);
-char				*check_backslash_quote(char *input, int i, t_minishell *shell);
+char				*check_backslash_quote(char *input, int i,
+					t_minishell *shell);
 int					ft_quotes_check_sub_double(char *str, int i,
 					int var, t_minishell *shell);
 
